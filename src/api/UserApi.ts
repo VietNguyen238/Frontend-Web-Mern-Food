a1b2137d0d9d2ac21020a108f25adee22b360b39
Nguyen Quoc Viet <vietdz2308@gmail.com>
2024-06-01 00:05:40 +0700

    Create function: Toast Notifications

--- src/api/UserApi.ts
+++ src/api/UserApi.ts
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "react-query";
+import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 type CreateUserRequest = {
@@ -66,7 +67,15 @@ export const useUpdateUser = () => {
     error,
     reset,
   } = useMutation(updateUserRequest);
-  console.log(isLoading);
 
-  return { updateUser, isLoading, isSuccess, isError, error, reset };
+  if (isSuccess) {
+    toast.success("User profile updated successfully!");
+  }
+
+  if (error) {
+    toast.error(error.toString());
+    reset();
+  }
+
+  return { updateUser, isLoading };
 };
