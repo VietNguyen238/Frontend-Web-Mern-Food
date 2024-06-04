f14beda828d39184b07352d06f99d0be17d75686
Nguyen Quoc Viet <vietdz2308@gmail.com>
2024-06-04 18:33:53 +0700

    Create function: Shows updates of the current user

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,8 +1,23 @@
-import { useUpdateUser } from "@/api/UserApi";
+import { useGetUser, useUpdateUser } from "@/api/UserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 export default function UserProfilePage() {
-  const { updateUser, isLoading } = useUpdateUser();
+  const { currentUser, isLoading: isGetLoading } = useGetUser();
+  const { updateUser, isLoading: isUpdateLoading } = useUpdateUser();
 
-  return <UserProfileForm onSave={updateUser} isLoading={isLoading} />;
+  if (isGetLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (!currentUser) {
+    return <span>Unable to load user profile</span>;
+  }
+
+  return (
+    <UserProfileForm
+      currentUser={currentUser}
+      onSave={updateUser}
+      isLoading={isUpdateLoading}
+    />
+  );
 }
