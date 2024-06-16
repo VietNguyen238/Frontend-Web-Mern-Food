91e6d4b8304a4cb4558353b7e24dc0960499ab63
Nguyen Quoc Viet <vietdz2308@gmail.com>
2024-06-16 22:00:04 +0700

    Update page: Create search API request

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,7 +1,20 @@
+import { useSearchRestaurant } from "@/api/RestaurantSearchApi";
 import { useParams } from "react-router-dom";
 
 export default function SearchPage() {
   const { city } = useParams();
+  const { results } = useSearchRestaurant();
 
-  return <span>User searched for {city}</span>;
+  return (
+    <span>
+      User searched for {city}
+      <span>
+        {results?.data.map((restaurant) => (
+          <span>
+            found - {restaurant.restaurantName}, {restaurant.city}
+          </span>
+        ))}
+      </span>
+    </span>
+  );
 }
